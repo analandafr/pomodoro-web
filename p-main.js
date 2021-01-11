698b0f41acbd62c4dc2ce92183e07e4c5fefd97d
analandafr <analandafr@gmail.com>
2021-01-10 21:00:11 -0300

    adaptando o arquivo 'main', inserindo botões

--- p-main.js
+++ p-main.js
@@ -1,78 +1,67 @@
 //notificação com som -> quando o Timer acabar
-function PomodoroAlarm() {
-    var TocarAlarme = document.getElementById('selecionar-a').value;
-    var TocarAudio = document.getElementById(TocarAlarme);
-    var ADvolume = document.getElementById('volume_a').value;
-    TocarAudio.volume = ADvolume/100;
-    TocarAudio.play();
+function playAlarm() {
+    var pomodoroAlarm =  new Audio("sound-mario.mp3");
+    pomodoroAlarm.play();
 }
 
-//crônometro
-function Cronometro(segundos, clicK) {
+//cronômetro na tela -> contagem regressiva do Pomodoro
+function TimerCronometro(segundos, a) {
     this.segundos = segundos || (25*60);
-    this.clicK = clicK || 500; 
-    this.clickFunction = [];
-    this.R = false;
-    this.C = this.segundos;
+    this.a = a || 500;
+    this.a = [];
+    this.timerRun = false;
+    this.timerC = this.segundos;
 
     this.start = function() {
-        if (this.R) {
+        if (this.timerRun) {
             return;
         }
 
-        this.R = true;
+        this.timerRun = true;
 
-        //começando timer
-        var ComecarTimer = Date.now(), 
-            Timer = this;
-
-        (function Click() {
-            segundosTimer = ((Date.now() - ComecarTimer)/1000) | 0;
-            var segundosC = Timer.C - segundosTimer;
-
-            //usuário interrompendo
-            if (Timer.R === false) {
-                Timer.C = segundosC;
+        //começando timer do Pomodoro
+        var ComecarPomodoro = Date.now(), 
+            Pomodoro = this;
+         
+        (function pomTimer() {
+            segundosComecando = ((Date.now() - ComecarPomodoro)/1000) | 0;
+            var segundostimerC = Pomodoro.timerC - segundosComecando;
+            
+            if (Pomodoro.timerRun === false) {
+                Pomodoro.timerC = segundostimerC;
             } else {
-                if (segundosC > 0) {
-                    setTimeout(Click, Timer.clicK);
+                if (segundostimerC > 0) {
+                    setTimeout(pomTimer, Pomodoro.a);
                 } else {
                     
-                    //parar
-                    Timer.C = 0;
-                    Timer.R = false;
+                    Pomodoro.timerC = 0;
+                    Pomodoro.timerRun = false;
 
                     //notificação
-                    PomodoroAlarm();
+                    pomodoroAlarm.play();
+                    
                 }
-
-                var timerC = parsesegundos(segundosC);
-                Timer.clickFunction.forEach(
-                    function(ClickFunction) {
-                      ClickFunction.call(this, 
-                      timerC.minutos, 
-                      timerC.segundos);
-                    },
-                Timer);
+                
+                var timetimerC = parsesegundos(segundostimerC);
+                Pomodoro.a.forEach(
+                    function(pomTimerFunction) {
+                        pomTimerFunction.call(this, 
+                        timetimerC.minutos, 
+                        timetimerC.segundos);
+                    }, 
+                Pomodoro);
             }
         }());        
     };
 
     //botão pausar timer
-    this.timerPausar = function() {
-        this.R = false;
+    this.pause = function() {
+        this.timerRun = false;
     };
 
-    //botão de reset -> reiniciar timer
-    this.timerReiniciar = function(segundos) {
-        this.R = false;
-        this.segundos = segundos
-        this.C = segundos
-    };
-
-    this.onClick = function(ClickFunction) {
-        if (typeof ClickFunction === 'function') {
-            this.clickFunction.push(ClickFunction);
+    this.onClick = function(pomTimerFunction) {
+        if (typeof pomTimerFunction === 'function') {
+            this.a.push(pomTimerFunction);
         }
     };
 }
@@ -85,94 +74,64 @@ function parsesegundos(segundos) {
 }
 
 window.onload = function () {
-    var TimerNaTela = document.getElementById('timer'),
-        customizarTimer = document.getElementById('btn-customz'),
-        timer = new Cronometro(),
-        defTimer = parsesegundos(25*60);
+    var cronometroTela = document.getElementById('timer'),
+        customzt = document.getElementById('customz'),
+        timer = new TimerCronometro(),
+        timerDef = parsesegundos(25*60);
+    
+    function PomodoroButtonsTela(minutos, segundos) {
+        Pminutos = minutos < 10 ? '0' + minutos : minutos;
+        Pminutos += ':';
+        Psegundos = segundos < 10 ? '0' + segundos : segundos;
 
-    function TimersButtons(minutos, segundos) {
-        if (minutos >= 60) {
-            horas = Math.floor(minutos / 60);
-            minutos = minutos % 60;
-            Cronometrohoras = horas + ':';
-            
-            
-        } else {
-            Cronometrohoras = '';
-        }
-        
-        Cronometrominutos = minutos < 10 ? '0' + minutos : minutos;
-        Cronometrominutos += ':';
-        Cronometrosegundos = segundos < 10 ? '0' + segundos : segundos;
-
-        TimerNaTela.textContent = Cronometrohoras + Cronometrominutos + Cronometrosegundos;
+        //P - ponteiros na tela, relógio etc
+        cronometroTela.textContent =  Pminutos + Psegundos; 
     }
-
-    function timerReiniciar2(segundos) {
-        timer.timerPausar();
-        timer = new Cronometro(segundos); 
-        timer.onClick(TimersButtons);
+    
+    //reinicia o timer quando clica em outro botão
+    function ResetButtonsTroca(segundos) {
+        timer.pause();
+        timer = new TimerCronometro(segundos); 
+        timer.onClick(PomodoroButtonsTela);
     }
+    
+    PomodoroButtonsTela(timerDef.minutos, timerDef.segundos);
 
-    TimersButtons(defTimer.minutos, defTimer.segundos);
-    timer.onClick(TimersButtons);
+    timer.onClick(PomodoroButtonsTela);
     
     //botões habilitados
-    document.getElementById('btn-start').addEventListener(
-        'click', function() { 
+    document.getElementById('btn_comecar').addEventListener(
+        'click', function () { 
             timer.start(); 
-            Pomodorosrealizados();
-            
         });
         
-    document.getElementById('btn-pause').addEventListener(
-        'click', function() {
-            timer.timerPausar(); 
-        });
-        
-    document.getElementById('btn-reset').addEventListener(
-        'click', function() {
-            timerReiniciar2(timer.segundos);
-            timer.start();
-            Pomodorosrealizados();
-            
-        });
-        
-    document.getElementById('btn-pomodoro').addEventListener(
+    document.getElementById('btn_pausar').addEventListener(
         'click', function () {
-            timerReiniciar2(25*60);
-            timer.start();
-            Pomodorosrealizados();
-            
+            timer.pause(); 
         });
         
-    document.getElementById('btn-intervalo').addEventListener(
+    document.getElementById('btn_pomodoro').addEventListener(
         'click', function () {
-            timerReiniciar2(5*60);
+            ResetButtonsTroca(25*60);
             timer.start();
-            Pomodorosrealizados();
-            
         });
         
-    document.getElementById('btn-intervalo2').addEventListener(
+    document.getElementById('btn_intervalo').addEventListener(
         'click', function () {
-            timerReiniciar2(10*60);
+            ResetButtonsTroca(5*60);
             timer.start();
-            
-            
         });
         
-    document.getElementById('btn-customz').addEventListener(
+    document.getElementById('btn_intervalolongo').addEventListener(
         'click', function () {
-            timerReiniciar2(customz.value*60);
+            ResetButtonsTroca(10*60);
             timer.start();
-            
-    });
-   
+        });
+        
+    document.getElementById('btn_custom').addEventListener(
+        'click', function () {
+            ResetButtonsTroca(customz.value*60);
+            timer.start();
+        });  
 };
-
-function Pomodorosrealizados(){
-    document.getElementById("Rpomodoros").innerHTML = `Pomodoros realizados: ${Rpomodoros}`;
-}
-
 
